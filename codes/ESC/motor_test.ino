3c9e3ef696cd48e75b9f23014101dc1c69a8a48d
likith K <49705166+likith3611@users.noreply.github.com>
2020-01-20 11:22:25 +0530

    Update motor_test.ino

--- codes/ESC/motor_test.ino
+++ codes/ESC/motor_test.ino
@@ -1,11 +1,11 @@
 #include <Servo.h>
-Servo motor1
-Servo motor2
+Servo motor1;
+Servo motor2;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  motor1.attach(D2);
-  motor2.attach(D3);
+  motor1.attach(2);
+  motor2.attach(3);
   motor1.writeMicroseconds(1000);
   motor2.writeMicroseconds(1000);
   
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int speed1=analogRead(A1);
-  speed2=map(speed1,0,1023,1000,2000);
+  int speed2=map(speed1,0,1023,1000,2000);
   motor1.writeMicroseconds(speed2); 
   motor2.writeMicroseconds(speed2);
 
